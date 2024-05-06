9e64baa1b26b7b03bd5ac63a97c31c1db56aa3b9
oscar <159032472+odgar@users.noreply.github.com>
2024-05-06 08:31:15 -0400

    bare minimum

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    playGedi();
+});
+
+
+//function to play correct audio
+function playGedi() {
+    var gedi = new Audio('assets/gedi.mp3');
+    gedi.play();
+};
+
+
 const canvas = document.getElementById('drawing-board');
 const ctx = canvas.getContext('2d');
 let isDrawing = false;
