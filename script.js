8c49535ca1155f0085f23e7aa5226ee34bc99b18
oscar <159032472+odgar@users.noreply.github.com>
2024-05-06 07:38:35 -0400

    forgot to commit last week

--- script.js
+++ script.js
@@ -12,45 +12,11 @@ canvas.addEventListener("mousedown", () => {
 });
 
 canvas.addEventListener("mousemove", e => {
-    if 
-})
-
-
-/*
-canvas.addEventListener('mousedown', () => {
-    ctx.beginPath();
-    ctx.arc(250, 250, 150, 0, 2 * Math.PI)
+    if(!isDrawing) return;
+    ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
-})
+});
 
-/*
-//rect = rectangle
-canvas.addEventListener('mouseup', () => {
-    ctx.clearRect(0,0,canvas.clientWidth, canvas.height);
-})
-
-/*
-//need to draw da rectangle
-ctx.lineTo(500, 190);
-ctx.lineTo(500, 400);
-ctx.lineTo(300, 400);
-ctx.lineTo(300,190);
-ctx.stroke(); 
-
-// triangle
-ctx.moveTo(400, 30);
-ctx.lineTo(400, 30);
-ctx.lineTo(500, 190);
-ctx.lineTo(300, 190);
-ctx.lineTo(400, 30);
-ctx.stroke();
-
-// door
-ctx.moveTo(435, 400);
-ctx.lineTo(435, 295);
-ctx.lineTo(435, 400);
-ctx.lineTo(435, 400);
-ctx.lineTo(365, 400);
-ctx.lineTo(365,295);
-ctx.lineTo(435,295);
-ctx.stroke(); */
+canvas.addEventListener("mouseup", () => {
+    isDrawing = false;
+});
