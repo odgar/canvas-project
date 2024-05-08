0157dd7ccc49137c32796aa3f88e51f34cfbe4e1
oscar <159032472+odgar@users.noreply.github.com>
2024-05-08 09:17:53 -0400

    replace js

--- script.js
+++ script.js
@@ -14,25 +14,77 @@ function playAll() {
 };
 
 
-const canvas = document.getElementById('drawing-board');
-const ctx = canvas.getContext('2d');
-let isDrawing = false;
-
-canvas.width = window.innerWidth;
+const canvas = document.getElementById("canvas");
+const body = document.querySelector("body");
 canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
 
-canvas.addEventListener("mousedown", () => {
-    isDrawing = true;
-    ctx.beginPath();
-    ctx.lineWidth = 5;
+var theColor = '';
+var lineW = 5;
+let prevX = null;
+let prevY = null;
+let draw = false;
+
+body.style.backgroundColor = "#FFFFFF";
+var theInput = document.getElementById("favcolor");
+
+const ctx = canvas.getContext("2d");
+ctx.lineWidth = lineW;
+
+document.getElementById("ageInputId").onInputId = function(){
+draw = null;
+document.getElementById("ageInputId").value;
+document.getElementById("ageOutputId").innerHTML = lineW;
+ctx.lineWidth = lineW;
+};
+
+let clrs = document.querySelectorAll(".clr");
+clrs = Array.from(clrs);
+clrs.forEach(clr => {
+    clr.addEventListener("click", () => {
+        ctx.strokeStyle = clr.dataset.clr;
+    })
+})
+
+let clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", () => {
+    ctx.clearRect(0,0, canvas.width, canvas.height)
 });
 
-canvas.addEventListener("mousemove", e => {
-    if(!isDrawing) return;
-    ctx.lineTo(e.offsetX, e.offsetY);
+let SaceBtn = document.querySelector("save");
+saveBtn.addEventListener("click", () => {
+    let data = canvas.toDataURL("imag/png");
+    let a = document.createElement("a");
+    a.href = data;
+    a.download = "sketch.png";
+    a.click();
+});
+
+window.addEventListener("mousedown", (e) => draw = true);
+window.addEventListener("mouseup", (e) => draw = false);
+
+window.addEventListener("mousedown", (e) => {
+    if(prevX == null || prevY == null || !draw){
+        prevX = e.clientX;
+        prevY = e.clientY;
+        return
+    }
+
+    let currentX = e.clientX;
+    let currentY = e.clientY;
+
+    ctx.beginpath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currentX, currentY);
     ctx.stroke();
+
+    prevX = currentX;
+    prevY = currentY
 });
 
-canvas.addEventListener("mouseup", () => {
-    isDrawing = false;
-});
+
+
+
+
+
+
